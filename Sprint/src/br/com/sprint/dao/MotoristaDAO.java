65f47dca4fb78cb01483d23ae05427148e76bee4
leothomazelli <70982787+leothomazelli@users.noreply.github.com>
2020-10-08 12:52:38 -0300

    Update MotoristaDAO.java

--- Sprint/src/br/com/sprint/dao/MotoristaDAO.java
+++ Sprint/src/br/com/sprint/dao/MotoristaDAO.java
@@ -28,30 +28,43 @@ public class MotoristaDAO {
     }
     
     public int adicionarMotorista(Motorista objeto) throws Exception {
-    	stmt = con.prepareStatement("INSERT INTO TB_MOTORISTA(NOME, EMAIL, TELEFONE, CNH, VALIDADE_CNH, SENHA, CNPJ) VALUES (?,?,?,?,?,?,?) returning id into ?; end;");
+    	stmt = con.prepareStatement("INSERT INTO TB_MOTORISTA(NOME, EMAIL, TELEFONE, SENHA) VALUES (?,?,?,?)");
     	stmt.setString(1, objeto.getNome());
     	stmt.setString(2, objeto.getEmail());
-    	//DÚVIDA (ORDEM DE INSERÇÃO OU NÃO) >  stmt.setString(3, objeto.getEndereco().getAll());
-    	stmt.setString(4, objeto.getTelefone());
-    	stmt.setString(5, objeto.getSenha());
-    	//DÚVIDA (=) > stmt.setString(6, objeto.getDocumentos().getAll());
-    	//DÚVIDA (=) > stmt.setString(7, objeto.getOcorrencia().getAll());
-    	
+    	stmt.setString(3, objeto.getTelefone());
+    	stmt.setString(4, objeto.getSenha());
     	return stmt.executeUpdate();
     	
     }
     
     public int atualizarTelefone(int id, String telefone) throws Exception {
     	stmt = con.prepareStatement("UPDATE TB_MOTORISTA SET TELEFONE = ? WHERE ID = ?");
-    	
     	stmt.setString(1, telefone);
     	stmt.setInt(2, id);
-    	
     	return stmt.executeUpdate();
     }
     
+    public Motorista getMotoristaById(int idMotorista) throws Exception {
+    	stmt = con.prepareStatement("SELECT * FROM TB_ENDERECO INNER JOIN TB_MOTORISTA ON (TB_ENDERECO.ID = TB_MOTORISTA.FK_ENDERECO) WHERE ID = ?");
+    	stmt.setInt(1, idMotorista);
+    	rset = stmt.executeQuery();
+    	Motorista objeto = new Motorista();
+    	if(rset.next()) {
+    		objeto.setIdMotorista(rset.getInt("ID_MOTORISTA"));
+    		objeto.setNome(rset.getString("NOME"));
+    		objeto.setEmail(rset.getString("EMAIL"));
+    		objeto.getEndereco().setLogradouro(rset.getString("LOGRADOURO"));
+    		objeto.setTelefone(rset.getString("TELEFONE"));
+    		objeto.setSenha(rset.getString("SENHA"));
+    		objeto.getDocumentos().setUrl(rset.getString("DOCUMENTO"));
+    		objeto.getOcorrencia().setIdSinistro(rset.getInt("SINISTRO_ID"));  // < DÃšVIDA NOME COLUNA (COMO VAMOS PUXAR O SINISTRO PELO SELECT)
+    		
+    	}
+    	return objeto;
+    }   
+    
     public Motorista getMotorista(String nome) throws Exception {
-    	stmt = con.prepareStatement("select * from tb_endereco inner join tb_motorista on (tb_endereco.id = tb_motorista.fk_endereco) where nome like ?");
+    	stmt = con.prepareStatement("SELECT * FROM TB_ENDERECO INNER JOIN TB_MOTORISTA ON (TB_ENDERECO.ID = TB_MOTORISTA.FK_ENDERECO) WHERE NOME LIKE ?");
     	stmt.setString(1, nome + "%");
     	rset = stmt.executeQuery();
     	Motorista objeto = new Motorista();
@@ -63,7 +76,7 @@ public class MotoristaDAO {
     		objeto.setTelefone(rset.getString("TELEFONE"));
     		objeto.setSenha(rset.getString("SENHA"));
     		objeto.getDocumentos().setUrl(rset.getString("DOCUMENTO"));
-    		objeto.getOcorrencia().setIdSinistro(rset.getInt("SINISTRO_ID"));  // < DÚVIDA NOME COLUNA (COMO VAMOS PUXAR O SINISTRO PELO SELECT)
+    		objeto.getOcorrencia().setIdSinistro(rset.getInt("SINISTRO_ID"));  // < DÃšVIDA NOME COLUNA (COMO VAMOS PUXAR O SINISTRO PELO SELECT)
     		
     	}
     	return objeto;
