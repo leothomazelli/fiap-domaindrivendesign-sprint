e102772bdffc98f48572c94f3116d6f2764363d7
suellenmarques <66932909+suellenmarques@users.noreply.github.com>
2020-10-30 18:28:03 -0300

    Update EnderecoDAO.java

--- Sprint/src/br/com/sprint/dao/EnderecoDAO.java
+++ Sprint/src/br/com/sprint/dao/EnderecoDAO.java
@@ -19,65 +19,56 @@ public class EnderecoDAO {
 	public EnderecoDAO() throws Exception{
 			con = Conexao.conectar();
 		}
-	public int deletePorId(int id) throws Exception{
-		stmt = con.prepareStatement("DELETE FROM TB_ENDERECO WHERE ID_ENDERECO = ?");
-		stmt.setDouble(1, id);
+	public int deletePorCep(int cep) throws Exception{
+		stmt = con.prepareStatement("DELETE FROM ENDERECOS WHERE CEP = ?");
+		stmt.setInt(1, cep);
 		return stmt.executeUpdate();
 	}
 	public int adicionarEndereco(Endereco objeto) throws Exception{
-		stmt = con.prepareStatement("INSERT INTO TB_ENDERECO(LOGRADOURO, CIDADE, UF, NUMERO, BAIRRO, CEP, COMPLEMENTO) VALUES(?,?,?,?,?,?,?)");
+		stmt = con.prepareStatement("INSERT INTO ENDERECOS(CEP, LOGRADOURO, CIDADE, UF, BAIRRO, COMPLEMENTO) VALUES(?,?,?,?,?,?)");
 		
-		stmt.setString(1, objeto.getLogradouro());
-		stmt.setString(2, objeto.getCidade());
-		stmt.setString(3, objeto.getUf());
-		stmt.setString(4, objeto.getNumero());
-		stmt.setString(5, objeto.getBairro());
-		stmt.setString(6, objeto.getCep());
+		stmt.setInt(1, objeto.getCep());
+		stmt.setString(2, objeto.getLogradouro());
+		stmt.setString(3, objeto.getCidade());
+		stmt.setString(4, objeto.getUf());
+		stmt.setString(6, objeto.getBairro());
 		stmt.setString(7, objeto.getComplemento());
-		
 		return stmt.executeUpdate();
 	}
-	public int atualizarLogradouro(String cep, String logradouro) throws Exception{
-		stmt = con.prepareStatement("UPDATE TB_ENDERECO SET LOGRADOURO = ? WHERE CEP = ?");
+	public int atualizarLogradouro(int cep, String logradouro) throws Exception{
+		stmt = con.prepareStatement("UPDATE ENDERECOS SET LOGRADOURO = ? WHERE CEP = ?");
 		
 		stmt.setString(1, logradouro);
-		stmt.setString(2, cep);
+		stmt.setInt(2, cep);
 		return stmt.executeUpdate();
 	}
-	
-	public Endereco getEnderecoById(int idEndereco) throws Exception{
-		stmt = con.prepareStatement("SELECT * FROM TB_ENDERECO WHERE CEP = ?");
+	public Endereco getEnderecoByCep(int cep) throws Exception{
+		stmt = con.prepareStatement("SELECT * FROM ENDERECOS WHERE CEP = ?");
 		
-		stmt.setInt(1, idEndereco);
+		stmt.setInt(1, cep);
 		rs = stmt.executeQuery();
 		Endereco obj = new Endereco();
 		if(rs.next()) {
 			obj.setLogradouro(rs.getString("LOGRADOURO"));
 			obj.setCidade(rs.getString("CIDADE"));
 			obj.setUf(rs.getString("UF"));
-			obj.setNumero(rs.getString("NUMERO"));
 			obj.setBairro(rs.getString("BAIRRO"));
-			obj.setCep(rs.getString("CEP"));
-			obj.setIdEndereco(rs.getInt("ID_ENDERECO"));
 			obj.setComplemento(rs.getString("COMPLEMENTO"));
 		}
 		return obj;
 	}
 	
-	public Endereco getEndereco(String cep) throws Exception{
-		stmt = con.prepareStatement("SELECT * FROM TB_ENDERECO WHERE CEP = ?");
+	public Endereco getEndereco(int cep) throws Exception{
+		stmt = con.prepareStatement("SELECT * FROM ENDERECOS WHERE CEP = ?");
 		
-		stmt.setString(1, cep);
+		stmt.setInt(1, cep);
 		rs = stmt.executeQuery();
 		Endereco objeto = new Endereco();
 		if(rs.next()) {
 			objeto.setLogradouro(rs.getString("LOGRADOURO"));
 			objeto.setCidade(rs.getString("CIDADE"));
 			objeto.setUf(rs.getString("UF"));
-			objeto.setNumero(rs.getString("NUMERO"));
 			objeto.setBairro(rs.getString("BAIRRO"));
-			objeto.setCep(rs.getString("CEP"));
-			objeto.setIdEndereco(rs.getInt("ID_ENDERECO"));
 			objeto.setComplemento(rs.getString("COMPLEMENTO"));
 		}
 		return objeto;
