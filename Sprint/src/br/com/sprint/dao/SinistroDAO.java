5807ff7120d77bcf3ef27fa043933ec7baad2ec5
suellenmarques <66932909+suellenmarques@users.noreply.github.com>
2020-10-29 21:41:50 -0300

    Update SinistroDAO.java

--- Sprint/src/br/com/sprint/dao/SinistroDAO.java
+++ Sprint/src/br/com/sprint/dao/SinistroDAO.java
@@ -3,6 +3,8 @@ package br.com.sprint.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
 
 import br.com.sprint.beans.Sinistro;
 import br.com.sprint.conexao.Conexao;
@@ -21,30 +23,30 @@ public class SinistroDAO {
 	}
 	
 	public int deletePorId(int idSinistro) throws Exception {
-		stmt = con.prepareStatement("DELETE FROM TB_SINISTRO WHERE ID_SINISTRO = ?");
+		stmt = con.prepareStatement("DELETE FROM SINISTROS WHERE ID_SINISTRO = ?");
 		stmt.setInt(1, idSinistro);
 		return stmt.executeUpdate();
 	}
 	
 	public int adicionarSinistro(Sinistro objeto) throws Exception {
-		stmt = con.prepareStatement("INSERT INTO TB_SINISTRO(DATA, HR_OCORRENCIA, DESC_EVENTO, DESC_DANOS, DOC_MOTORISTA) values (?,?,?,?,?)");
-		stmt.setString(1, objeto.getData());
-		stmt.setString(2, objeto.getHorarioDaOcorrencia());
-		stmt.setString(3, objeto.getDescricaoEvento());
-		stmt.setString(4, objeto.getDescricaoDanos());
-		//DÚVIDA (ORDEM DE INSERÇÃO OU NÃO) > stmt.setString(5, objeto.getDocumentosMotorista().getUrl());
+		stmt = con.prepareStatement("INSERT INTO SINISTROS(ID_MOTORISTA, DT_SINISTRO, HR_OCORRENCIA, DESC_EVENTO, DESC_DANOS) values (?,?,?,?,?)");
+		stmt.setInt(1, objeto.getMotorista().getIdMotorista());
+		stmt.setString(2, objeto.getData());
+		stmt.setString(3, objeto.getHorarioDaOcorrencia());
+		stmt.setString(4, objeto.getDescricaoEvento());
+		stmt.setString(5, objeto.getDescricaoDanos());
 		return stmt.executeUpdate();
 	}
 	
 	public int atualizarDescricaoEvento(String descricaoEvento, int idSinistro) throws Exception {
-		stmt = con.prepareStatement("UPDATE TB_SINISTRO SET DESC_EVENTO = ? WHERE ID = ?");
+		stmt = con.prepareStatement("UPDATE SINISTROS SET DESC_EVENTO = ? WHERE ID_SINISTRO = ?");
 		stmt.setString(1, descricaoEvento);
 		stmt.setInt(2, idSinistro);
 		return stmt.executeUpdate();
 	}
 	
 	public Sinistro getSinistro(int idSinistro) throws Exception {
-		stmt = con.prepareStatement("SELECT * FROM TB_SINISTRO WHERE ID = ?");
+		stmt = con.prepareStatement("SELECT * FROM MOTORISTAS INNER JOIN SINISTROS ON (MOTORISTAS.ID_MOTORISTA = SINISTROS.ID_MOTORISTA) WHERE ID_SINISTRO = ?");
 		stmt.setInt(1, idSinistro);
 		rset = stmt.executeQuery();
 		Sinistro objeto = new Sinistro();
@@ -54,8 +56,16 @@ public class SinistroDAO {
 			objeto.setHorarioDaOcorrencia(rset.getString("HR_OCORRENCIA"));
 			objeto.setDescricaoEvento(rset.getString("DESC_EVENTO"));
 			objeto.setDescricaoDanos(rset.getString("DESC_DANOS"));
-			objeto.getDocumentosMotorista().setUrl(rset.getString("URL"));
+			objeto.getMotorista().setIdMotorista(rset.getInt("ID_MOTORISTA"));
+
+			/*String dataRecebida = "23/11/2015";
+			SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+
+			Date dataFormatada = formato.parse(dataRecebida);
+			Date data = formato.parse("23/11/2015");
+			objeto.setData(data);*/
 		}
 		return objeto;
 	}
 }
+
