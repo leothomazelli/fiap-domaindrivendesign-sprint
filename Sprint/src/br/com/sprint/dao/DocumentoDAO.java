86c1ec65b6ba0b34ec9874e4f621a302782afcd1
suellenmarques <66932909+suellenmarques@users.noreply.github.com>
2020-10-30 17:33:25 -0300

    Update DocumentoDAO.java

--- Sprint/src/br/com/sprint/dao/DocumentoDAO.java
+++ Sprint/src/br/com/sprint/dao/DocumentoDAO.java
@@ -3,7 +3,6 @@ package br.com.sprint.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-
 import br.com.sprint.beans.Documento;
 import br.com.sprint.conexao.Conexao;
 
@@ -21,30 +20,31 @@ public class DocumentoDAO {
 	}
 	
 	public int deletePorId(int id) throws Exception {
-		stmt = con.prepareStatement("DELETE FROM TB_DOCUMENTO WHERE ID_DOCUMENTO = ?");
+		stmt = con.prepareStatement("DELETE FROM DOCUMENTOS WHERE ID_DOCUMENTO = ?");
 		stmt.setInt(1, id);
 		return stmt.executeUpdate();
 	}
 	
 	public int adicionarDocumento(Documento objeto) throws Exception {
-		stmt = con.prepareStatement("INSERT INTO TB_DOCUMENTO(TIPO, URL, DT_CRIACAO, ULTIMA_MODIFICACAO, DT_VENCIMENTO) values (?,?,?,?,?)");
-		stmt.setString(1, objeto.getTipo());
-		stmt.setString(2, objeto.getUrl());
-		stmt.setString(3, objeto.getDataCriacao());
-		stmt.setString(4, objeto.getUltimaModificacao());
-		stmt.setString(5, objeto.getDataVencimento());
+		stmt = con.prepareStatement("INSERT INTO DOCUMENTOS(DOCUMENTO_MOTORISTA, ID_DOCUMENTO, TIPO, URL, ULTIMA_MODIFICACAO, DT_VENCIMENTO) values (?,?,?,?,?,?)");
+		stmt.setInt(1, objeto.getMotorista().getIdMotorista());
+		stmt.setInt(2, objeto.getIdDocumento());
+		stmt.setString(3, objeto.getTipo());
+		stmt.setString(4, objeto.getUrl());
+		stmt.setString(6, objeto.getUltimaModificacao());
+		stmt.setString(7, objeto.getDtVencimento());
 		return stmt.executeUpdate();
 	}
 	
-	public int atualizarVencimento(String url, String vencimento) throws Exception {
-		stmt = con.prepareStatement("UPDATE TB_DOCUMENTO SET VENCIMENTO = ? WHERE URL = ?");
-		stmt.setString(1, vencimento);
+	public int atualizarDocumento(String url, String DtVencimento) throws Exception {
+		stmt = con.prepareStatement("UPDATE DOCUMENTOS SET DT_VENCIMENTO = ? WHERE URL = ?");
+		stmt.setString(1, DtVencimento);
 		stmt.setString(2, url);
 		return stmt.executeUpdate();
 	}
 	
 	public Documento getDocumento(int idDocumento) throws Exception {
-		stmt = con.prepareStatement("SELECT * FROM TB_DOCUMENTO WHERE ID_DOCUMENTO = ?");
+		stmt = con.prepareStatement("SELECT * FROM DOCUMENTOS WHERE ID_DOCUMENTO = ?");
 		stmt.setInt(1, idDocumento);
 		rset = stmt.executeQuery();
 		Documento objeto = new Documento();
@@ -52,9 +52,9 @@ public class DocumentoDAO {
 			objeto.setIdDocumento(rset.getInt("ID_DOCUMENTO"));
 			objeto.setTipo(rset.getString("TIPO"));
 			objeto.setUrl(rset.getString("URL"));
-			objeto.setDataCriacao(rset.getString("DT_CRIACAO"));
 			objeto.setUltimaModificacao(rset.getString("ULTIMA_MODIFICACAO"));
-			objeto.setDataVencimento(rset.getString("DT_VENCIMENTO"));
+			objeto.setDtVencimento(rset.getString("DT_VENCIMENTO"));
+			
 		}
 		return objeto;
 	}
