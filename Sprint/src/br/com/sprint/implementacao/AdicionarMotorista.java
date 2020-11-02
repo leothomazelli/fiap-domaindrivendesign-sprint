f0a94b800639931055cd8d79a080efe382aa0571
suellenmarques <66932909+suellenmarques@users.noreply.github.com>
2020-11-02 17:03:50 -0300

    Update AdicionarMotorista.java

--- Sprint/src/br/com/sprint/implementacao/AdicionarMotorista.java
+++ Sprint/src/br/com/sprint/implementacao/AdicionarMotorista.java
@@ -1,7 +1,6 @@
 package br.com.sprint.implementacao;
 
 import javax.swing.JOptionPane;
-
 import br.com.sprint.beans.Motorista;
 import br.com.sprint.dao.MotoristaDAO;
 import br.com.sprint.excecao.Excecoes;
@@ -15,7 +14,10 @@ public class AdicionarMotorista {
 			System.out.println("Motoristas adicionados: " + dao.adicionarMotorista(new Motorista(JOptionPane.showInputDialog("Informe o nome do motorista").toUpperCase(),
 																								 JOptionPane.showInputDialog("Informe o email").toUpperCase(),
 																								 JOptionPane.showInputDialog("Informe o telefone"),
-																								 JOptionPane.showInputDialog("Informe a senha"))));
+																								 JOptionPane.showInputDialog("Informe a senha"),
+																								 JOptionPane.showInputDialog("Informe o número do logradouro"),
+																								 JOptionPane.showInputDialog("Informe o cep")
+																								 )));
 			dao.close();
 		}
 		catch (Exception e) {
