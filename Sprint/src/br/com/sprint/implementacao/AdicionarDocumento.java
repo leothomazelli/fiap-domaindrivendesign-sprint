5edf9765a83adc92761fe09e3c1207790107f94e
suellenmarques <66932909+suellenmarques@users.noreply.github.com>
2020-11-03 21:32:15 -0300

    Update AdicionarDocumento.java

--- Sprint/src/br/com/sprint/implementacao/AdicionarDocumento.java
+++ Sprint/src/br/com/sprint/implementacao/AdicionarDocumento.java
@@ -3,7 +3,9 @@ package br.com.sprint.implementacao;
 import javax.swing.JOptionPane;
 
 import br.com.sprint.beans.Documento;
+import br.com.sprint.beans.Motorista;
 import br.com.sprint.dao.DocumentoDAO;
+import br.com.sprint.dao.MotoristaDAO;
 import br.com.sprint.excecao.Excecoes;
 
 public class AdicionarDocumento {
@@ -12,11 +14,22 @@ public class AdicionarDocumento {
 		
 		try {
 			DocumentoDAO dao = new DocumentoDAO();
-			System.out.println("Documentos adicionados: " + dao.adicionarDocumento(new Documento(JOptionPane.showInputDialog("Informe o tipo do documento").toUpperCase(),
-																							     JOptionPane.showInputDialog("Informe a url"),
-																							     JOptionPane.showInputDialog("Informe a data de criação"),
-																							     JOptionPane.showInputDialog("Informe a ultima modificação"),
-																							     JOptionPane.showInputDialog("Informe a data de vencimento"))));
+			MotoristaDAO motoristaDAO = new MotoristaDAO();
+			
+			Documento documento = new Documento();
+			Motorista motorista = new Motorista();
+			motorista = motoristaDAO.getMotoristaById(Integer.parseInt(JOptionPane.showInputDialog("Informe o ID do motorista")));
+			documento.setIdDocumento(Integer.parseInt(JOptionPane.showInputDialog("Informe o id do documento")));
+			documento.setTipo(JOptionPane.showInputDialog("Informe o tipo do documento").toUpperCase());
+			documento.setUrl(JOptionPane.showInputDialog("Informe a url do documento").toLowerCase());
+			documento.setUltimaModificacao(JOptionPane.showInputDialog("Informe a data de hoje"));
+			documento.setDtVencimento(JOptionPane.showInputDialog("Informe a data de vencimento do documento"));
+			documento.setMotorista(motorista);
+			
+			System.out.println("Documentos adicionados: " + dao.adicionarDocumento(documento));
+			
+			
+			
 			dao.close();
 		}
 		catch (Exception e) {
